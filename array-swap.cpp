9a1d13269260b65b96db7dffd79a799bf8752aa1
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2025-09-07 21:52:07 +0500

    Update array-swap.cpp

--- array-swap.cpp
+++ array-swap.cpp
@@ -4,7 +4,11 @@ using namespace std;
 void swapAlternate(int arr[], int size) {
     for(int i = 0; i < size; i += 2) {
         if(i + 1 < size) {
-            swap(arr[i], arr[i + 1]);
+            // swap(arr[i], arr[i + 1]);
+            // Instead of using the built in swap fuction you can mannually do it by the following code.
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = temp;
         }
     }
 }
