731f8750f7dec41ea8e22a3022358ff99d6af2a8
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2025-09-09 21:38:51 +0500

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    bool isSwap = false;
+
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                isSwap = true;
+            }
+        }
+
+        if(!isSwap) { // Array is already sorted
+            return;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int n = 5;
+    int arr[] = {4, 1, 5, 2, 3};
+
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
