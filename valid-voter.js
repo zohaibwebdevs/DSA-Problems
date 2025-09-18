4ef4109852b2432d47188c4735b084a2e0a154b1
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2025-09-18 20:58:42 +0500

    Update valid-voter.js

--- valid-voter.js
+++ valid-voter.js
@@ -1,3 +1,5 @@
+// If you want to run this code, link this js file with your html file and then open it in the browser, then you will see the result.
+
 let ans = Number(prompt("What is your age?")); // Using Number function will prevent the user from putting something other than number.
 
 if(isNaN(ans)) { // isNaN function check if the value of ans is NaN.
